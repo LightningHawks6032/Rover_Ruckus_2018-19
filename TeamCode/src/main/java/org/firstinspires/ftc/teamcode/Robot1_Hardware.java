d31c374a0f8c351808421f23cbef2cd393ad2d38
Mich-T <mitoryu@students.natickps.org>
2018-10-10 15:52:43 -0400

    New Motors
    declares the Linear Actuator and slide motor, as well as the slide motor's direction. These are not yet integrated into TeleOp. The linear actuator is currently declared as a Servo, as we didn't see anything else for it.
    - Mich

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot1_Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot1_Hardware.java
@@ -3,12 +3,17 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Robot1_Hardware {
     // Declaring the motors
     DcMotor leftDrive = null;
     DcMotor rightDrive = null;
     DcMotor middleDrive = null;
+    DcMotor slideMotor = null;
+    Servo linearActuator = null;
+    //
+    //
 
 
     public Robot1_Hardware(HardwareMap hardwareMap) {
@@ -16,7 +21,8 @@ public class Robot1_Hardware {
         leftDrive = hardwareMap.get(DcMotor.class, "ld");
         rightDrive = hardwareMap.get(DcMotor.class, "rd");
         middleDrive = hardwareMap.get(DcMotor.class, "md");
-
+        slideMotor = hardwareMap.get(DcMotor.class, "sm");
+        linearActuator = hardwareMap.get(Servo.class, "la");
     }
 
     public void initHardware() {
@@ -24,6 +30,7 @@ public class Robot1_Hardware {
         leftDrive.setDirection(DcMotor.Direction.FORWARD);
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
         middleDrive.setDirection(DcMotor.Direction.REVERSE);
+        slideMotor.setDirection(DcMotor.Direction.FORWARD);
     }
 
 }
