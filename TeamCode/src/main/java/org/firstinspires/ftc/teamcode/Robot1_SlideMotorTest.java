b988646afb756fe8a66cfc03c1e2e4d7db1df312
Owen Sorber <38570999+owsorber@users.noreply.github.com>
2018-10-18 15:51:32 -0400

    Added Slide Motor test program
    Test for the optimal ratio between slow and fast slide motors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot1_SlideMotorTest.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Robot1 Slide Test", group="Iterative Opmode")
+public class Robot1_SlideMotorTest extends OpMode {
+    Robot1_Hardware hardware;
+
+    double powerCoefficient = 0.3; // we're testing this value
+
+    double redMotorPower = 1.0; // RED = FAST
+    double blueMotorPower = redMotorPower * powerCoefficient;
+
+    public void init() {
+        //Initialize hardware
+        hardware = new Robot1_Hardware(hardwareMap);
+        hardware.initHardware();
+    }
+
+    public void loop() {
+        redMotorPower = -gamepad1.left_stick_y;
+        blueMotorPower = redMotorPower * powerCoefficient;
+
+        if (gamepad1.dpad_down)
+            powerCoefficient -= 0.005;
+        else if (gamepad1.dpad_up)
+            powerCoefficient += 0.005;
+
+        if (powerCoefficient <= 0) {
+            powerCoefficient = 0;
+        }
+        if (powerCoefficient >= 1) {
+            powerCoefficient = 1;
+        }
+
+
+        // Update motor power
+        hardware.fastSlideMotor.setPower(redMotorPower);
+        hardware.slowSlideMotor.setPower(blueMotorPower);
+
+
+        // Print to telemetry
+        telemetry.addLine("Up/down on dpad for 0.05 increase/decrease in power coefficient");
+        telemetry.addData("Fast (Red) Motor", hardware.slowSlideMotor.getPower());
+        telemetry.addData("Slow (Blue) Motor", hardware.slowSlideMotor.getPower());
+        telemetry.addData("Power Coefficient (Ratio between slow and fast power)", powerCoefficient);
+        telemetry.update();
+    }
+}
