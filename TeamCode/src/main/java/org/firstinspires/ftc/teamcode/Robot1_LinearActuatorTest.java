ceff04f60625e1f639d2233242fd06337c9f5290
Mich-T <mitoryu@students.natickps.org>
2018-10-23 16:01:56 -0400

    update to actuator test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot1_LinearActuatorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot1_LinearActuatorTest.java
@@ -12,6 +12,14 @@ public class Robot1_LinearActuatorTest extends OpMode {
     }
 
     public void loop() {
-        
+        testActuator();
+    }
+
+    private void testActuator(){
+        if(gamepad1.dpad_up){
+            hardware.linearActuator.setPosition(1);
+        }else if(gamepad1.dpad_down){
+            hardware.linearActuator.setPosition(0);
+        }
     }
 }
