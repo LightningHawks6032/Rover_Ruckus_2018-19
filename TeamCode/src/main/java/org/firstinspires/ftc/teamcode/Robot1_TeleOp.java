da13a0632fe03447408cfbc0cfdda5a0d2799580
Owen Sorber <38570999+owsorber@users.noreply.github.com>
2018-10-25 20:40:01 -0400

    Added comment about potential changes for manageClaws()
    It may be beneficial to add a variable for claw position and then write claw.setPosition(clawPos) at the end of the method. This may be why the claws weren't working as of Wednesday. We can test this next meeting.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot1_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot1_TeleOp.java
@@ -154,6 +154,28 @@ public class Robot1_TeleOp extends OpMode {
         } else{
             hardware.rightClaw.setPosition(0);
         }
+
+        /**
+         * Note: may be beneficial to add a variable for claw position and then say claw.setPosition(clawPos)
+         * This may be why the claws weren't working as of Wednesday
+         * So, we could say (we'll test this next time if the claws still aren't working):
+
+         if (gamepad2.left_trigger > 0.5) {
+            leftClawPos = 0;
+         } else{
+            leftClawPos = 1;
+         }
+
+         // Right claw
+         if (gamepad2.right_trigger > 0.5){
+            rightClawPos = 1;
+         } else{
+            rightClawPos = 0;
+         }
+
+         hardware.leftClaw.setPosition(leftClawPos);
+         hardware.rightClaw.setPosition(rightClawPos);
+         */
     }
 
     // linear actuator control
