4819cef80b86c3a7aee7341788976d1c8fe000de
Owen Sorber <38570999+owsorber@users.noreply.github.com>
2018-10-30 15:02:58 -0400

    New Test for Linear Actuator

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot1_LinearActuatorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot1_LinearActuatorTest.java
@@ -7,26 +7,48 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 public class Robot1_LinearActuatorTest extends OpMode {
     Robot1_Hardware hardware;
 
+    double actuPos;
+
     public void init() {
         //Initialize hardware
         hardware = new Robot1_Hardware(hardwareMap);
         hardware.initHardware();
+        actuPos = 0.5;
     }
 
     public void loop() {
         testActuator();
         telemetry.addLine("This program tests the linear actuator as a servo");
         telemetry.addData("Linear actuator position: ",hardware.linearActuator.getPosition());
+        telemetry.addData("Actupos variable", actuPos);
         telemetry.update();
     }
 
     private void testActuator(){
-        if(gamepad1.dpad_up){
-            hardware.linearActuator.setPosition(1);
-        }else if(gamepad1.dpad_down){
-            hardware.linearActuator.setPosition(0);
+        /*if(gamepad2.dpad_up) {
+            actuPos += 0.01;
+        }else if(gamepad2.dpad_down) {
+            actuPos -= 0.01;
+        }else{
+            // I'm hoping this will be able to make the linear actuator sit still - Mich
+            actuPos = hardware.linearActuator.getPosition();
+            if(actuPos < 0) {
+                actuPos = 0;
+            }
+            if(actuPos > 1){
+                actuPos = 1;
+            }
+        }*/
+
+        actuPos += gamepad2.right_stick_y * 0.01;
+
+        if(actuPos > 1){
+            actuPos = 1;
+        }
+        if(actuPos < 0){
+            actuPos = 0;
         }
 
-
+        hardware.linearActuator.setPosition(actuPos);
     }
 }
