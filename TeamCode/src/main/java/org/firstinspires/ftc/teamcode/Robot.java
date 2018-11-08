d5b2ef0482aaaf3d29f33b33fa6b60242009c5f2
Owen Sorber <38570999+owsorber@users.noreply.github.com>
2018-11-08 13:44:07 -0500

    Robot class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,20 @@
+/**
+ * This class is an unfinished Robot class which will store the robot's methods all in one place.
+ * We may or may not use this, just an idea.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.util.Hardware;
+
+public class Robot {
+    private String name;
+    private Hardware hardware;
+
+    public Robot(String nm, Hardware hware) {
+        name = nm;
+        hardware = hware;
+    }
+
+
+}
