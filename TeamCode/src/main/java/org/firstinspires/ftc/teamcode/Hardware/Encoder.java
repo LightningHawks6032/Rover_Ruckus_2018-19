7a9d27eda1ff200d785b464cb4ba06f3a8924e1c
Owen Sorber <38570999+owsorber@users.noreply.github.com>
2018-11-11 16:01:32 -0500

    Encoder class for more effective implementation of encoders in auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Encoder.java
@@ -0,0 +1,78 @@
+/**
+ * Encoder class; properties are motor, type of encoder (Neverest/Tetrix), and wheel diameter
+ *
+ * Should be instantiated in opmodes that use encoders for more effective implementation.
+ */
+
+package org.firstinspires.ftc.teamcode.Hardware;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Encoder {
+    private DcMotor motor; // motor
+    private String type; // Either "Neverest" or "Tetrix"
+    private double wheelDiameter; // wheel diameter in inches
+
+
+    final static int NEVEREST_TICKS_PER_REV = 1120;
+    final static int TETRIX_TICKS_PER_REV = 1440;
+
+    public Encoder(DcMotor motor, String type, double diam) {
+        this.motor = motor;
+        this.type = type;
+        wheelDiameter = diam;
+    }
+
+    public int ticksPerRev() {
+        if (type.equals("Neverest"))
+            return NEVEREST_TICKS_PER_REV;
+        else if (type.equals("Tetrix"))
+            return TETRIX_TICKS_PER_REV;
+        else
+            return NEVEREST_TICKS_PER_REV; // default
+    }
+
+    public int getEncoderCount() {
+        return motor.getCurrentPosition();
+    }
+
+    public double motorRotations() {
+        return getEncoderCount() / ticksPerRev();
+    }
+
+    public double linDistance() {
+        return motorRotations() * getWheelCircumference();
+    }
+
+
+    // Shortcut methods for resetting, run without, run to position, and setting target position
+    public void reset() {
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+    public void runWithout() {
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+    public void runToPosition() {
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+    public void setTarget(double linDistance) {
+        int encoderPos = (int) (linDistance * ticksPerRev() / getWheelCircumference());
+        motor.setTargetPosition(encoderPos);
+    }
+
+
+    // Accessor methods
+    public DcMotor getMotor() {
+        return motor;
+    }
+    public String getType() {
+        return type;
+    }
+    public double getWheelDiameter() {
+        return wheelDiameter;
+    }
+    public double getWheelCircumference() {
+        return 3.14159 * wheelDiameter;
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -8,9 +8,21 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.util.Hardware;
 
+import org.firstinspires.ftc.teamcode.Hardware.HardwareInterface;
+
 public abstract class Robot {
+    private String name;
+    private HardwareInterface hardware;
+
+
+    public Robot(String name, HardwareInterface hardware) {
+        this.name = name;
+        this.hardware = hardware;
+    }
 
     public abstract void manageDrivetrain();
-
     public abstract void manageManips();
+
+
+
 }
