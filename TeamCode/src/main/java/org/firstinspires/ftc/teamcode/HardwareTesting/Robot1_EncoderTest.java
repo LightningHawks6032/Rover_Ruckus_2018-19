7ccf05d74f5a2b7f3d20765fafb5c428ff6b4050
Owen Sorber <38570999+owsorber@users.noreply.github.com>
2018-11-26 11:10:57 -0500

    Deleted unecessary code given that encoder programming now works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareTesting/Robot1_EncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareTesting/Robot1_EncoderTest.java
@@ -20,23 +20,17 @@ public class Robot1_EncoderTest extends OpMode {
 
         leftDriveEncoder = hardware.drivetrain.getLeftEncoder();
         rightDriveEncoder = hardware.drivetrain.getRightEncoder();
-        /*leftDriveEncoder = new Encoder(hardware.leftDrive, "Neverest", 4);
-        rightDriveEncoder = new Encoder(hardware.rightDrive, "Neverest", 4);*/
-        leftDriveEncoder.reset();
-        rightDriveEncoder.reset();
-        leftDriveEncoder.runWith();
-        rightDriveEncoder.runWith();
     }
 
     public void loop() {
         hardware.drivetrain.manageTeleOp();
+
+        // debugging
         double avg = (leftDriveEncoder.linDistance() + rightDriveEncoder.linDistance())/2;
         telemetry.addData("Left Encoder count: ", leftDriveEncoder.getEncoderCount());
         telemetry.addData("Left Motor rotations: ", leftDriveEncoder.motorRotations());
         telemetry.addData("Right Encoder count: ", rightDriveEncoder.getEncoderCount());
         telemetry.addData("Right Motor rotations: ", rightDriveEncoder.motorRotations());
-        //telemetry.addData("Left Encoder count: ", hardware.leftDrive.getCurrentPosition());
-        //telemetry.addData("Right Encoder count: ", hardware.rightDrive.getCurrentPosition());
         telemetry.addData("Distance: ", avg);
         telemetry.update();
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot1_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot1_TeleOp.java
@@ -44,8 +44,6 @@ public class Robot1_TeleOp extends OpMode {
 
     public void loop() {
         hardware.drivetrain.manageTeleOp();
-        //manageDrivetrain();
-        //applyBoost();
         manageSlide();
         manageWinch();
         manageClaws();
@@ -57,75 +55,6 @@ public class Robot1_TeleOp extends OpMode {
         telemetry.addData("linear actuator position", hardware.linearActuator.getPosition());
         telemetry.update();
     }
-
-    /*
-    private void applyBoost() {
-        if (gamepad1.x)
-            boost = 1;
-    }
-
-    // Shortcut method for setting the power of the left drive, right drive, and middle drive
-    private void setPowers(double lp, double rp, double mp) {
-        leftPower = lp;
-        rightPower = rp;
-        middlePower = mp;
-    }
-
-    private void basicDrive() {
-        //drive1 controls (slide drive)
-        if (gamepad1.left_trigger > 0) {
-            setPowers(0, 0, -MAX_MIDDLE_POWER); // strafe left
-        } else if (gamepad1.right_trigger > 0) {
-            setPowers(0, 0, MAX_MIDDLE_POWER); // strafe right
-        } else {
-            // motor power = joysticks
-            setPowers(-gamepad1.left_stick_y * MAX_DRIVE_POWER, -gamepad1.right_stick_y * MAX_DRIVE_POWER, 0);
-        }
-    }
-
-
-    private void planetaryDrive() {
-        // drive2 controls (planetary drive)
-        // drive 2: electric boogaloo
-        if (gamepad1.left_trigger == 0 && gamepad1.right_trigger == 0) {
-            //Moving forward, backward, left, right if not touching triggers
-            setPowers(-gamepad1.left_stick_y * MAX_DRIVE_POWER,
-                      -gamepad1.left_stick_y * MAX_DRIVE_POWER,
-                      gamepad1.left_stick_x * MAX_MIDDLE_POWER);
-        } else {
-            // Turning
-            if (gamepad1.left_trigger > 0) {
-                setPowers(-MAX_DRIVE_POWER, MAX_DRIVE_POWER, 0);
-            } else if (gamepad1.right_trigger > 0) {
-                setPowers(MAX_DRIVE_POWER, -MAX_DRIVE_POWER, 0);
-            }
-
-        }
-    }
-
-    private void manageMode() {
-        if (gamepad1.left_bumper)
-            driveMode = 1;
-        if (gamepad1.right_bumper)
-            driveMode = 2;
-    }
-
-    // MAIN DRIVETRAIN METHOD
-    private void manageDrivetrain() {
-        // sets drive mode
-        manageMode();
-
-        if (driveMode == 1) {
-            basicDrive();
-        } else {
-            planetaryDrive();
-        }
-
-
-        hardware.leftDrive.setPower(boost * leftPower);
-        hardware.rightDrive.setPower(boost * rightPower);
-        hardware.middleDrive.setPower(boost * middlePower * 2);
-    }*/
 
     // horizontal slide control
     private void manageSlide() {
