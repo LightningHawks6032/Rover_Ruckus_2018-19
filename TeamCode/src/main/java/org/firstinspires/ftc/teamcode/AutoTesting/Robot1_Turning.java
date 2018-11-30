45e24b63daa6a25ac7323f4da23bde15dbea0049
Owen Sorber <38570999+owsorber@users.noreply.github.com>
2018-11-30 14:04:31 -0500

    Testing turns with gyro

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTesting/Robot1_Turning.java
@@ -0,0 +1,25 @@
+/**
+ * Basic auto class to test the turn() function(s) in the drivetrain class which use gyro
+ */
+
+package org.firstinspires.ftc.teamcode.AutoTesting;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Hardware.Robot1_Hardware;
+
+@Autonomous(name="Robot 1 Turning with Gyro", group="Linear Opmode")
+public class Robot1_Turning extends LinearOpMode {
+    private Robot1_Hardware hardware;
+
+    public void runOpMode() {
+        hardware = new Robot1_Hardware(hardwareMap, gamepad1);
+        hardware.initHardware();
+
+        waitForStart();
+
+        hardware.drivetrain.turn(90, true);
+        hardware.drivetrain.turn(90, false);
+    }
+}
