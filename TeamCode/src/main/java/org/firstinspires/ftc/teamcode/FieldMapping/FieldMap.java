e914b598e6df2245242c1188977e745bdd41947a
Mich-T <mitoryu@students.natickps.org>
2018-12-04 14:45:45 -0500

    Q3 and Q4
    mappp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldMapping/FieldMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldMapping/FieldMap.java
@@ -22,6 +22,16 @@ public class FieldMap {
         addElement(2, "Left Mineral", new Vector(-46.67, 23.33));
         addElement(2, "Middle Mineral", new Vector(-35, 35));
         addElement(2, "Right Mineral", new Vector(-23.33, 46.67));
+
+        // Quadrant 3 Minerals
+        addElement(3, "Left Mineral", new Vector(-23.33, -46.67));
+        addElement(3, "Middle Mineral", new Vector(-35, -35));
+        addElement(3, "Right Mineral", new Vector(-46.67, -23.33));
+
+        // Quadrant 4 Minerals
+        addElement(4, "Left Mineral", new Vector(46.67, -23.33));
+        addElement(4, "Middle Mineral", new Vector(35, -35));
+        addElement(4, "Right Mineral", new Vector(23.33, -46.67));
     }
 
     // Accesses the position of a specific element based on quadrant and name
