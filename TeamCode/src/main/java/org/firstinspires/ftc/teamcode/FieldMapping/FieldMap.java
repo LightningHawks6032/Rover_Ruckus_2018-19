80c3b577ad7dc9a14ff7578139c7d7580e6fbde8
Mich-T <mitoryu@students.natickps.org>
2018-12-05 13:48:08 -0500

    Map fixed (again)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldMapping/FieldMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldMapping/FieldMap.java
@@ -20,46 +20,46 @@ public class FieldMap {
 
     private void generate() {
 
-        // Quadrant 1
-        // Minerals
-        addElement(1, "Left Mineral", new Vector(2*SQUARE_LENGTH, SQUARE_LENGTH));
-        addElement(1, "Middle Mineral", new Vector(MID_QUADRANT, MID_QUADRANT));
-        addElement(1, "Right Mineral", new Vector(SQUARE_LENGTH, 2*SQUARE_LENGTH));
-        // Blue Depot
-        addElement(1, "Blue Depot", new Vector(5*HALF_SQUARE_LENGTH,5*HALF_SQUARE_LENGTH));
-
         // Quadrant 2
         // Minerals
-        addElement(2, "Left Mineral", new Vector(SQUARE_LENGTH, -2*SQUARE_LENGTH));
-        addElement(2, "Middle Mineral", new Vector(MID_QUADRANT, -MID_QUADRANT));
-        addElement(2, "Right Mineral", new Vector(2*SQUARE_LENGTH, -SQUARE_LENGTH));
-        // Crater Positions
-        addElement(2,"Crater Left Edge", new Vector(SQUARE_LENGTH,-5*HALF_SQUARE_LENGTH));
-        addElement(2,"Crater Right Edge", new Vector(5*HALF_SQUARE_LENGTH,-SQUARE_LENGTH));
-        addElement(2,"Crater Center", new Vector(5*HALF_SQUARE_LENGTH,-5*HALF_SQUARE_LENGTH));
+        addElement(2, "Left Mineral", new Vector(-2*SQUARE_LENGTH, SQUARE_LENGTH));
+        addElement(2, "Middle Mineral", new Vector(-MID_QUADRANT, MID_QUADRANT));
+        addElement(2, "Right Mineral", new Vector(-SQUARE_LENGTH, 2*SQUARE_LENGTH));
+        // Blue Depot
+        addElement(2, "Blue Depot", new Vector(-5*HALF_SQUARE_LENGTH,5*HALF_SQUARE_LENGTH));
 
         // Quadrant 3
         // Minerals
-        addElement(3, "Left Mineral", new Vector(-2*SQUARE_LENGTH, -SQUARE_LENGTH));
+        addElement(3, "Left Mineral", new Vector(-SQUARE_LENGTH, -2*SQUARE_LENGTH));
         addElement(3, "Middle Mineral", new Vector(-MID_QUADRANT, -MID_QUADRANT));
-        addElement(3, "Right Mineral", new Vector(-SQUARE_LENGTH, -2*SQUARE_LENGTH));
-        // Red Depot
-        addElement(3, "Red Depot", new Vector(-5*HALF_SQUARE_LENGTH,-5*HALF_SQUARE_LENGTH));
+        addElement(3, "Right Mineral", new Vector(-2*SQUARE_LENGTH, -SQUARE_LENGTH));
+        // Crater Position
+        addElement(3,"Crater Left Edge", new Vector(-SQUARE_LENGTH,-5*HALF_SQUARE_LENGTH));
+        addElement(3,"Crater Right Edge", new Vector(-5*HALF_SQUARE_LENGTH,-SQUARE_LENGTH));
+        addElement(3,"Crater Center", new Vector(-5*HALF_SQUARE_LENGTH,-5*HALF_SQUARE_LENGTH));
 
         // Quadrant 4
         // Minerals
-        addElement(4, "Left Mineral", new Vector(-SQUARE_LENGTH, 2*SQUARE_LENGTH));
-        addElement(4, "Middle Mineral", new Vector(-MID_QUADRANT, MID_QUADRANT));
-        addElement(4, "Right Mineral", new Vector(-2*SQUARE_LENGTH, SQUARE_LENGTH));
+        addElement(4, "Left Mineral", new Vector(2*SQUARE_LENGTH, -SQUARE_LENGTH));
+        addElement(4, "Middle Mineral", new Vector(MID_QUADRANT, -MID_QUADRANT));
+        addElement(4, "Right Mineral", new Vector(SQUARE_LENGTH, -2*SQUARE_LENGTH));
+        // Red Depot
+        addElement(4, "Red Depot", new Vector(5*HALF_SQUARE_LENGTH,-5*HALF_SQUARE_LENGTH));
+
+        // Quadrant 1
+        // Minerals
+        addElement(1, "Left Mineral", new Vector(SQUARE_LENGTH, 2*SQUARE_LENGTH));
+        addElement(1, "Middle Mineral", new Vector(MID_QUADRANT, MID_QUADRANT));
+        addElement(1, "Right Mineral", new Vector(2*SQUARE_LENGTH, SQUARE_LENGTH));
         // Crater Positions
-        addElement(4,"Crater Left Edge", new Vector(-SQUARE_LENGTH,5*HALF_SQUARE_LENGTH));
-        addElement(4,"Crater Right Edge", new Vector(-5*HALF_SQUARE_LENGTH,SQUARE_LENGTH));
-        addElement(4,"Crater Center", new Vector(-5*HALF_SQUARE_LENGTH,5*HALF_SQUARE_LENGTH));
+        addElement(1,"Crater Left Edge", new Vector(SQUARE_LENGTH,5*HALF_SQUARE_LENGTH));
+        addElement(1,"Crater Right Edge", new Vector(5*HALF_SQUARE_LENGTH,SQUARE_LENGTH));
+        addElement(1,"Crater Center", new Vector(5*HALF_SQUARE_LENGTH,5*HALF_SQUARE_LENGTH));
 
         // Nav Targets
         addElement(0, "Blue Rover", new Vector(0,QUADRANT_LENGTH));
-        addElement(0, "Front Craters", new Vector(-QUADRANT_LENGTH,0));
-        addElement(0, "Back Space", new Vector(QUADRANT_LENGTH,0));
+        addElement(0, "Front Craters", new Vector(QUADRANT_LENGTH,0));
+        addElement(0, "Back Space", new Vector(-QUADRANT_LENGTH,0));
         addElement(0, "Red Footprint", new Vector(0,-QUADRANT_LENGTH));
     }
 
