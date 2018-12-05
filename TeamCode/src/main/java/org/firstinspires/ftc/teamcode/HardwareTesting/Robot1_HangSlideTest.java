ec4570485fd0c1150151a15d01e3cd189f00aa92
Owen Sorber <38570999+owsorber@users.noreply.github.com>
2018-12-05 14:36:47 -0500

    Enabled hanging test as a tele-op

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareTesting/Robot1_HangSlideTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareTesting/Robot1_HangSlideTest.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.HardwareTesting;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Hardware.Robot1_Hardware;
 
+@TeleOp(name="Robot 1 Hanging Test", group="Iterative Opmode")
 public class Robot1_HangSlideTest extends OpMode {
     private Robot1_Hardware hardware;
     private double vexPow = 0, neverestPow = 0;
