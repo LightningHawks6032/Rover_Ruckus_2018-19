dbaa0720fa2e99945c59ba8bd5df1354615cd8c4
Owen Sorber <38570999+owsorber@users.noreply.github.com>
2019-02-05 12:37:51 -0500

    Reworked omnislidedrive code into new mecanum wheel drive class
    Tele-op controls completed based on https://www.roboteq.com/images/article-images/frontpage/wheel-rotations.jpg
    
    Also added basic driveDistance

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/MecanumWheelDrive.java
@@ -0,0 +1,192 @@
+package org.firstinspires.ftc.teamcode.Hardware;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.teamcode.AutonomousData;
+
+public class MecanumWheelDrive implements RobotHardware {
+    public DcMotor leftFront;
+    public DcMotor rightFront;
+    public DcMotor leftBack;
+    public DcMotor rightBack;
+    public Encoder leftFrontEncoder;
+    public Encoder rightFrontEncoder;
+    public Encoder leftBackEncoder;
+    public Encoder rightBackEncoder;
+    public ExpansionHubIMU imu;
+    public MRGyro gyro;
+    private Gamepad gamepad;
+
+    private int wheelDiameter = 5;
+
+
+    // AUTO BASED VARIABLES
+    private LinearOpMode autonomous = null; // stays null unless used in an auto
+    public long startTime;
+
+    public MecanumWheelDrive(DcMotor lf, DcMotor rf, DcMotor lb, DcMotor rb, ExpansionHubIMU hubIMU, MRGyro gyroSensor, Gamepad gamepad) {
+        leftFront = lf;
+        rightFront = rf;
+        leftBack = lb;
+        rightBack = rb;
+        imu = hubIMU;
+        gyro = gyroSensor;
+        this.gamepad = gamepad;
+
+        leftFrontEncoder = new Encoder(lf, AutonomousData.NEVEREST_20_ENCODER, wheelDiameter);
+        rightFrontEncoder = new Encoder(rf, AutonomousData.NEVEREST_20_ENCODER, wheelDiameter);
+        leftBackEncoder = new Encoder(lb, AutonomousData.NEVEREST_20_ENCODER, wheelDiameter);
+        rightBackEncoder = new Encoder(rb, AutonomousData.NEVEREST_20_ENCODER, wheelDiameter);
+    }
+
+    public void setStartTime(long time) {
+        startTime = time;
+    }
+    public void setAuto(LinearOpMode auto) {
+        autonomous = auto;
+    }
+
+    public void initHardware() {
+        leftFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftBack.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightBack.setDirection(DcMotorSimple.Direction.FORWARD);
+        encoderSetup();
+    }
+
+    private void encoderSetup() {
+        leftFrontEncoder.setup();
+        rightFrontEncoder.setup();
+        leftBackEncoder.setup();
+        rightBackEncoder.setup();
+    }
+
+    private void resetEncoders() {
+        leftFrontEncoder.reset();
+        rightFrontEncoder.reset();
+        leftBackEncoder.reset();
+        rightBackEncoder.reset();
+    }
+
+    private void runWithoutEncoders() {
+        leftFrontEncoder.runWithout();
+        rightFrontEncoder.runWithout();
+        leftBackEncoder.runWithout();
+        rightBackEncoder.runWithout();
+    }
+
+    private void runEncodersToPosition() {
+        leftFrontEncoder.runToPosition();
+        rightFrontEncoder.runToPosition();
+        leftBackEncoder.runToPosition();
+        rightBackEncoder.runToPosition();
+    }
+
+    // Shortcut method for setting the power of the motors
+    public void setPowers(double lfp, double rfp, double lbp, double rbp) {
+        leftFront.setPower(lfp);
+        rightFront.setPower(rfp);
+        leftBack.setPower(lbp);
+        rightBack.setPower(rbp);
+    }
+
+    public void manageTeleOp() {
+        if (gamepad.left_stick_y == 0 && gamepad.right_stick_y == 0) {
+            if (gamepad.right_trigger > 0) {
+                manageStrafing(true);
+            } else if (gamepad.left_trigger > 0) {
+                manageStrafing(false);
+            }
+        } else {
+            if (gamepad.right_trigger > 0) {
+                manageDiagonalStrafing(true);
+            } else if (gamepad.left_trigger > 0) {
+                manageDiagonalStrafing(false);
+            } else {
+                setPowers(-gamepad.left_stick_y, -gamepad.right_stick_y, -gamepad.left_stick_y, -gamepad.right_stick_y);
+            }
+        }
+    }
+
+    private void manageStrafing(boolean right) {
+        double pow;
+        if (right) {
+            pow = gamepad.right_trigger;
+            setPowers(pow, -pow, -pow, pow);
+        } else {
+            pow = gamepad.left_trigger;
+            setPowers(-pow, pow, pow, -pow);
+        }
+    }
+
+    private void manageDiagonalStrafing(boolean right) {
+        double pow;
+        if (right) {
+            pow = -gamepad.right_stick_y;
+            if (pow > 0)
+                setPowers(pow, 0, 0, pow);
+            else
+                setPowers(0, -pow, -pow, 0);
+        } else {
+            pow = -gamepad.left_stick_y;
+            if (pow > 0)
+                setPowers(0, pow, pow, 0);
+            else
+                setPowers(-pow, 0, 0, -pow);
+        }
+    }
+
+    /**
+     * Robot drives forward or backward a set amount of linear distance using encoders
+     * @param direction : forward (1) or backward (-1)
+     * @param distance : linear distance in inches for the robot to drive over
+     * @param pow : constant power at which the robot drives
+     */
+    public void driveDistance(int direction, double distance, double pow) throws InterruptedException {
+        resetEncoders();
+        runEncodersToPosition();
+
+        leftFrontEncoder.setTarget(direction * distance);
+        rightFrontEncoder.setTarget(direction * distance);
+        leftBackEncoder.setTarget(direction * distance);
+        rightBackEncoder.setTarget(direction * distance);
+
+        setPowers(direction * pow, direction * pow, direction * pow, direction * pow);
+
+        while (leftFront.isBusy() && rightFront.isBusy() && leftBack.isBusy() && rightBack.isBusy() && autoRunning()) {
+            // WAIT - Motors are busy
+        }
+
+        setPowers(0, 0, 0, 0);
+        runWithoutEncoders();
+        //updateAngleFromIMU();
+    }
+
+
+    // Accessor methods
+    public double getLeftFrontPower() {
+        return leftFront.getPower();
+    }
+    public double getRightFrontPower() {
+        return rightFront.getPower();
+    }
+    public double getLeftBackPower() {
+        return leftBack.getPower();
+    }
+    public double getRightBackPower() {
+        return rightBack.getPower();
+    }
+    public double getAverageDist() {
+        double sum = leftFrontEncoder.linDistance() + rightFrontEncoder.linDistance() + leftBackEncoder.linDistance() + rightBackEncoder.linDistance();
+        return sum / 4;
+    }
+
+
+    // Used to break all while loops when an opmode stops
+    private boolean autoRunning() {
+        return System.currentTimeMillis() - startTime <= AutonomousData.TIME_LIMIT && !autonomous.isStopRequested();
+    }
+}
