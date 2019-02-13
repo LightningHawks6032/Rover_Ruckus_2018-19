454c8ffdae819340146b5e2eba8e362c4652c6b4
Mich-T <mitoryu@students.natickps.org>
2019-02-13 17:14:56 -0500

    intake teleOp functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/StatesBot_Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/StatesBot_Intake.java
@@ -47,8 +47,58 @@ public class StatesBot_Intake {
 
     //TeleOp functions
 
+    public void manageTeleOp() {
+        manageHarvester();
+        manageFlipper();
+        manageSlide();
+    }
+
+    private void manageHarvester(){
+        //Yet again foiled by hexcore motor
+        if (gamepad.a) {
+            //harvester.setPower(HARVESTER_POWER);
+        } else if (gamepad.y) {
+            //harvester.setPower(-HARVESTER_POWER);
+        } else {
+            //harvester.setPower(0);
+        }
+
+    }
+
+
+    private void manageFlipper(){
+        //this may not allow you to change direction mid-movement - micc
+        if(gamepad.x && flipper.getPosition() == FLIPPER_IN_VAL){
+            flipper.setPosition(FLIPPER_OUT_VAL);
+        }else if(gamepad.x && flipper.getPosition() == FLIPPER_OUT_VAL){
+            flipper.setPosition(FLIPPER_IN_VAL);
+        }
+    }
+
+    // Manage horizontal slide
+    private boolean autoRetract = false; // Should the horizontal slide be attempting to auto retract?
+    private void manageSlide() {
+        double pow = -gamepad.left_stick_y;
+        int encoderVal = slideEncoder.getEncoderCount();
+
+        if ((pow > 0 && encoderVal < HORIZONTAL_SLIDE_MAX) || (pow < 0 && encoderVal > HORIZONTAL_SLIDE_MIN)) {
+            autoRetract = false;
+            horizontalSlide.setPower(pow);
+        } else if (autoRetract && encoderVal > HORIZONTAL_SLIDE_MIN) {
+            horizontalSlide.setPower(-1);
+        } else {
+            horizontalSlide.setPower(0);
+        }
+
+        if (gamepad.left_bumper)
+            autoRetract = true;
+
+    }
 
     //Auto functions
+
+    //nothing here as of now, as harvester is a core hex motor which we haven't figured out, and flipper is a servo
+    //that can be accessed directly in auto without need for functions
 
 
 }
